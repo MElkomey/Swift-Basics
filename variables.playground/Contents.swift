326bfe115fa1e1a72d5e648b71fe9455c835617d
MElkomey <mm6231169@gmail.com>
2023-06-07 15:10:42 -0700

    variables and constants added

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+//variables
+var firstName = "Mohamed" , age : Int = 5
+firstName="Ahmed"
+
+var middleName :String
+middleName="Ali"
+
+var lastName :String = "Eslam"
+
+var x = 5
+//casting
+x = Int(3.5)
+
+var y :Float = 3.5
+y = 7
+
+var z : Double = 5.554587545456
+
+var hasSale : Bool = true
+hasSale = false
+
+print(firstName)
+print(middleName + " " + lastName); print(Double(y) + z)
+print(age)
+
+//Constants
+let familyMembers : Int = 10
+///familyMembers = 30  ///error
+let hasPhone :Bool
+hasPhone = false
+////hasPhone = true   /////error
+
+print("HasPhone: " + String(hasPhone))
